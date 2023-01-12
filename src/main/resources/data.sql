4ba0f47c495302e7cf8c0c0cd8fe06f1210b314d
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-01-12 15:30:29 +0100

    v

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,11 @@
 INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
-VALUES ('Labradoodle', 'Charlie', '01 / 01 / 2019');
+VALUES ('Golden Retriever', 'Lucy', '02 / 14 / 2019');
 
 INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
 VALUES ('German Shepherd', 'Max', '01 / 15 / 2019 ');
 
 INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
 VALUES ('Poodle', 'Bella', '02 / 02 / 2019 ');
-
-INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
-VALUES ('Golden Retriever', 'Lucy', '02 / 14 / 2019');
 
 INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
 VALUES ('Cocker Spaniel', 'Daisy', '03 / 01 / 2019');
@@ -23,10 +20,10 @@ INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
 VALUES ('Boxer', 'Molly', '04 / 14 / 2019');
 
 INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
-VALUES ('Beagle', 'Toby', '05 / 01 / 2019');
+VALUES ('Labradoodle', 'Charlie', '01 / 01 / 2019');
 
 INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
-VALUES ('Yorkshire Terrier', 'Daisy', '05 / 15 / 2019');
+VALUES ('Beagle', 'Toby', '05 / 01 / 2019');
 
 INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
 VALUES ('Rottweiler', 'Rocky', '06 / 02 / 2019');
@@ -36,23 +33,3 @@ VALUES ('Pit Bull', 'Buddy', '06 / 14 / 2019');
 
 INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
 VALUES ('Siberian Husky', 'Sasha', '07 / 01 / 2019');
-
-INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
-VALUES ('Dachshund', 'Lola', '07 / 15 / 2019');
-
-INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
-VALUES ('Pomeranian', 'Teddy', '08 / 02 / 2019');
-
-INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
-VALUES ('Australian Shepherd', 'Maddie', '08 / 14 / 2019');
-
-INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
-VALUES ('Dalmatian', 'Spot', '09 / 01 / 2019');
-
-INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
-VALUES ('Shih Tzu', 'Cocoa', '09 / 15 / 2019');
-
-INSERT INTO Puppies (puppy_breed, puppy_name, puppy_birthday)
-VALUES ('Bichon Frise', 'Tulip', '10 / 02 / 2019');
-
-
